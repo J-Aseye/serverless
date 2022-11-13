01f72673fedd661c566fa167b7099bd0b1cb377b
Joyce Abena Gbadago <aseyegbadago@gmail.com>
2022-11-13 01:56:23 +0000

    Update config.ts
    Created a new auth for the application

--- client/src/config.ts
+++ client/src/config.ts
@@ -6,6 +6,6 @@ export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
   domain: 'dev-3yftz3lm.us.auth0.com',            // Auth0 domain
-  clientId: 'M6V1ElIGLn783QdhlljivdKQHBgdGcAa',          // Auth0 client id
+  clientId: 'GeDG9jU4AEuV19AJLQKfP5Hc9etOweHt',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
